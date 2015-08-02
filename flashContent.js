df2181109785e751225aa9b30aa69f6de5743fb1
Andreas Botsikas <andreasbotsikas@gmail.com>
2015-08-02 14:28:19 +0300

    Initial content from the old host

---
+++ flashContent.js
@@ -0,0 +1,7 @@
+document.write("<object classid='clsid:d27cdb6e-ae6d-11cf-96b8-444553540000' codebase='http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0' width='700' height='500' id='WebSiteTemplate' align='middle'>");
+      document.write("<param name='allowScriptAccess' value='sameDomain' />");
+      document.write("<param name='movie' value='ComeSoon.swf' />");
+      document.write("<param name='quality' value='high' />");
+      document.write("<param name='bgcolor' value='#0036CE' />");
+      document.write("<embed src='ComeSoon.swf' quality='high' bgcolor='#0036CE' width='700' height='500' name='WebSiteTemplate' align='middle' allowScriptAccess='sameDomain' type='application/x-shockwave-flash' pluginspage='http://www.macromedia.com/go/getflashplayer' />    ");
+    document.write("</object>");
